98893ce7c34e377340f36dedbb4a09adc1694e23
prayagyadav <prayagyadav206@gmail.com>
2024-11-28 11:33:00 +0530

    speed-test
    Former-commit-id: ee565204a039399ee93f2d6c809f82fd92018e99
    Former-commit-id: 569f1c1559ff64b215bce969194cd5893ce572e9
    Former-commit-id: 2f2f4bab9bd322f85a0f1b0238eb379a3ae94778 [formerly b8d9f0c9284b15bb5daec2ce3fcdf30270dc28b2] [formerly 92d88bfd1d35bb6d763d4a0f70130166b65d3638 [formerly 35aa13fbc1d6ffdeb6ac26b233b9401f8a488f4c]]
    Former-commit-id: 18335e68dc5d6f375c90b46b2e230464a8b286ac [formerly addc8013648385485134f900046fb7177105ffcd]
    Former-commit-id: 403a8fc2d7ffeb303f696697a479653cd9955a9f

---
+++ speed-test/processor.py
@@ -0,0 +1,92 @@
+from coffea import processor
+from coffea.analysis_tools import PackedSelection, Cutflow
+import awkward as ak
+import pandas as pd
+import dask_awkward as dak
+import hist.dask as hda
+import numpy as np
+from collections import namedtuple
+import hist
+import fastjet
+import vector
+vector.register_awkward()
+from config import plots
+import sys
+import os
+local_dir = os.environ['LOCAL_DIR']
+sys.path.append(local_dir)
+import scripts
+from scripts.analyzers import ReconstructedParticle as ReconstructedParticleUtil
+from scripts.analyzers import Jet as JetUtil
+
+plot_props = pd.DataFrame(plots)
+
+def get_1Dhist(name, var, flatten=False):
+    '''
+    name: eg. Zm
+    var: eg. variable containing array of mass of Z
+    flatten: If to flatten var before fill; True by default
+    Returns a histogram
+    '''
+    props = plot_props[name]
+    if flatten : var = dak.ravel(var) # Removes None values and all the nesting
+    var = var[~dak.is_none(var, axis=0)] # Remove None values only
+    return hda.Hist.new.Reg(props.bins, props.xmin, props.xmax).Double().fill(var)
+
+
+#################################
+#Begin the processor definition #
+#################################
+class jetclustering(processor.ProcessorABC):
+    '''
+    Processor: Define actual calculations here
+    '''
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def process(self,events):
+        
+        # Object Selections
+        Muons = events.ReconstructedParticles.match_collection(events.Muonidx0)
+        sel_muon_p_gt_25 = Muons.p > 25.0
+        Muons = Muons[sel_muon_p_gt_25]
+        Z = ReconstructedParticleUtil.resonanceBuilder(Muons, 91.0)
+        Recoil = ReconstructedParticleUtil.recoilBuilder(Z, 240.0)
+        
+        #Event Selections
+        cuts = PackedSelection()
+        cuts.add("n_gte_2_Muons", ak.num(Muons, axis=1) >= 2 )
+        cuts.add("m_gt_70_Z", Z.m > 70.0 )
+        cuts.add("m_lt_100_Z", Z.m < 100.0 )
+        cuts.add("p_gt_20_Z", Z.p > 20.0 )
+        cuts.add("p_lt_70_Z", Z.p < 70.0 )
+        cuts.add("m_gt_120_Recoil", Recoil.m > 120.0 )
+        cuts.add("m_lt_140_Recoil", Recoil.m < 140.0 )
+
+        # Apply the event selections
+        Good_Z = Z[cuts.all()]
+        Good_Recoil = Recoil[cuts.all()]
+        
+        #Prepare output
+        #Choose the required histograms and their assigned variables to fill
+        names = plot_props.columns.to_list()
+        vars_sel = [
+            Good_Recoil.m,
+            Good_Z.p,
+            Good_Z.m,
+        ]
+        sel_ocl = cuts.cutflow(*cuts.names).yieldhist()
+
+        Output = {
+            'histograms': {
+                'sel':{name:get_1Dhist(name,var,flatten=False) for name,var in zip(names,vars_sel)},
+            },
+            'cutflow': {
+                'sel': {'Onecut':sel_ocl[0],'Cutflow':sel_ocl[1],'Labels':sel_ocl[2]},
+            }
+        }
+        
+        return Output
+
+    def postprocess(self, accumulator):
+        pass
