10483d2128e9466a8c20d3ee17a8f549334b0c95
Prayag Yadav <prayagyadav206@gmail.com>
2025-01-01 17:47:53 +0530

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_nefertiti'
+#html_theme = 'sphinx_nefertiti'
 html_static_path = ['_static']
 
 # -- Theme options ------------------------------------------------------------
