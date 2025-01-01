5104714f806162f17a06d3f6b3765a3adeb61136
Prayag Yadav <prayagyadav206@gmail.com>
2025-01-01 17:53:11 +0530

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -24,20 +24,20 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'sphinx_nefertiti'
+# html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_nefertiti'
 html_static_path = ['_static']
 
 # -- Theme options ------------------------------------------------------------
 # https://sphinx-nefertiti.readthedocs.io/en/latest/quick-start.html#customize-the-theme
-html_theme_options = {
-  "repository_name": "prayagyadav/coffea-fcc-analyses",
-  "repository_url": "https://github.com/prayagyadav/coffea-fcc-analyses",
-  "footer_links": ",".join([
-    "Documentation|https://prayagyadav.github.io/coffea-fcc-analyses",
-    "Repository|https://github.com/prayagyadav/coffea-fcc-analyses",
-    "Issues|https://github.com/prayagyadav/coffea-fcc-analyses/issues",
-    "COFFEA|https://github.com/CoffeaTeam/coffea",
-    "FCCAnalyses|https://github.com/HEP-FCC/FCCAnalyses"
-  ])
-}
+#html_theme_options = {
+#  "repository_name": "prayagyadav/coffea-fcc-analyses",
+ # "repository_url": "https://github.com/prayagyadav/coffea-fcc-analyses",
+ # "footer_links": ",".join([
+  #  "Documentation|https://prayagyadav.github.io/coffea-fcc-analyses",
+  #  "Repository|https://github.com/prayagyadav/coffea-fcc-analyses",
+ #   "Issues|https://github.com/prayagyadav/coffea-fcc-analyses/issues",
+  #  "COFFEA|https://github.com/CoffeaTeam/coffea",
+ #   "FCCAnalyses|https://github.com/HEP-FCC/FCCAnalyses"
+#  ])
+#}
