4ab51d669e2a224a7cf35b0b1d9d56f45cc9b488
Prayag Yadav <prayagyadav206@gmail.com>
2025-01-02 11:36:42 +0530

    Update conf.py
    Former-commit-id: 0be3334efef76a994b4d992c2aff1ff77a920599
    Former-commit-id: 333b56813f5414e1a45a9fad4c1dc197f7148068
    Former-commit-id: 7f4a0d13c4d981d31e00068d938f79efea541df5 [formerly e213631d5705454cc78111f890d19a2928a8045a] [formerly 876e787a1a892101db2ae81854e71a0d0aee77b8 [formerly 7a9425e4d616ea78ba2fb1a3569be6a1a9770783]]
    Former-commit-id: e2cca09513b2acce81b10b59d8b5bcad470710b4 [formerly 00bfe7cbaa8170861c7874b938562e9ab4c2d213]
    Former-commit-id: eb15cd63b545dd31c1057d82e5ed318b39595f73

--- doc/conf.py
+++ doc/conf.py
@@ -28,6 +28,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = "pydata_sphinx_theme"
 
 html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
 
 # -- Theme options ------------------------------------------------------------
 # https://sphinx-nefertiti.readthedocs.io/en/latest/quick-start.html#customize-the-theme
