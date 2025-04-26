e185d7f0c495967db14c8f8262812854bd0a7cf3
prayagyadav <prayagyadav206@gmail.com>
2025-04-26 18:58:04 +0200

    install

--- setup.sh
+++ setup.sh
@@ -5,3 +5,18 @@ if [ "${0}" != "${BASH_SOURCE}" ]; then
 	export PYTHONPATH="${LOCAL_DIR}/scripts:${PYTHONPATH}"
 	alias coffea-fcc-analyses="${LOCAL_DIR}/bin/coffea-fcc-analyses"
 fi
+
+# Install plugins
+cd ${LOCAL_DIR}/scripts/plugins
+plugins=("fastjet")
+
+# Loop through the array
+for plugin in "${plugins[@]}"
+do
+    echo "Installing $plugin ..."
+    cd $plugin
+    make
+    cd ..
+done
+
+cd ${LOCAL_DIR}
