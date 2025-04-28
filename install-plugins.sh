7412520302b11aa717942a054fde5d9daa107276
prayagyadav <prayagyadav206@gmail.com>
2025-04-28 13:36:11 +0200

    Correct LD path

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,11 +1,10 @@
 if [ "${0}" != "${BASH_SOURCE}" ]; then
-	# Determinig the location of this setup script
-	export LOCAL_DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd)
+  # Determinig the location of this setup script
+  export LOCAL_DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd)
 
-	export PYTHONPATH="${LOCAL_DIR}/scripts:${PYTHONPATH}"
-	alias coffea-fcc-analyses="${LOCAL_DIR}/bin/coffea-fcc-analyses"
+  export PYTHONPATH="${LOCAL_DIR}/scripts:${PYTHONPATH}"
+  alias coffea-fcc-analyses="${LOCAL_DIR}/bin/coffea-fcc-analyses"
 fi
-
 
 # As a note, remember to have the build time dependencies on your system
 # For fastjet they are :
@@ -16,49 +15,34 @@ fi
 REQUIRED_PACKAGES=("fastjet")
 
 check_and_install_package() {
-    package=$1
-    if ! python3 -c "import ${package}" &> /dev/null; then
-        echo "${package} is not installed. Installing..."
-        pip3 install "$package"
-    else
-        echo "${package} is already installed."
-    fi
+  package=$1
+  if ! python3 -c "import ${package}" &>/dev/null; then
+    echo "${package} is not installed. Installing..."
+    pip3 install "$package"
+  else
+    echo "${package} is already installed."
+  fi
 }
 
 echo "Installing dependencies ..."
 # Check and install each package
 for package in "${REQUIRED_PACKAGES[@]}"; do
-    check_and_install_package "$package"
+  check_and_install_package "$package"
 done
-
-
-
-# Temporarily install swig within the container
-# cd /tmp
-# wget https://github.com/swig/swig/archive/refs/tags/v4.1.1.tar.gz
-# tar -xzf v4.1.1.tar.gz
-# cd swig-4.1.1
-
-# ./autogen.sh
-# ./configure --prefix=$HOME/.local
-# make -j$(nproc)
-# make install
-
-# Now make sure $HOME/.local/bin is in your PATH:
-# export PATH=$HOME/.local/bin:$PATH
-
 
 # Install plugins
 cd ${LOCAL_DIR}/scripts/plugins
 plugins=("fastjet")
+plugins_ld_path=("$(python -c 'import fastjet; print(fastjet.__path__[0])')/_fastjet_core/lib")
 
 # Loop through the array
-for plugin in "${plugins[@]}"
-do
-    echo "Installing Plugin: $plugin ..."
-    cd $plugin
-    bash build.sh
-    cd ..
+for plugin in "${plugins[@]}"; do
+  echo "Installing Plugin: $plugin ..."
+  cd $plugin
+  bash build.sh
+  echo "exporting ${plugins_ld_path[@]} to LD_LIBRARY_PATH}"
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${plugins_ld_path[@]}
+  cd ..
 done
 
 cd ${LOCAL_DIR}
--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 if [ "${0}" != "${BASH_SOURCE}" ]; then
-	# Determinig the location of this setup script
-	export LOCAL_DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd)
+  # Determinig the location of this setup script
+  export LOCAL_DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd)
 
-	export PYTHONPATH="${LOCAL_DIR}/scripts:${PYTHONPATH}"
-	alias coffea-fcc-analyses="${LOCAL_DIR}/bin/coffea-fcc-analyses"
+  export PYTHONPATH="${LOCAL_DIR}/scripts:${PYTHONPATH}"
+  alias coffea-fcc-analyses="${LOCAL_DIR}/bin/coffea-fcc-analyses"
+
+  # Plugin stuff ...
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(python -c 'import fastjet; print(fastjet.__path__[0])')/_fastjet_core/lib
 fi
