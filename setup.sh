709fb1c0a04b4db318e08db28ba843dd57be86a9
prayagyadav <prayagyadav206@gmail.com>
2025-04-30 14:57:16 +0200

    check if singularity is available

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ "${0}" != "${BASH_SOURCE}" ]; then
     alias coffea-fcc-analyses="${LOCAL_DIR}/bin/coffea-fcc-analyses"
 
     # Plugin stuff ...
-    if [ -d /cvmfs/unpacked.cern.ch/registry.hub.docker.com ]; then
+    if [ -d /cvmfs/unpacked.cern.ch/registry.hub.docker.com] && command -v singularity &> /dev/null; then
         export COFFEA_IMAGE_PATH=/cvmfs/unpacked.cern.ch/registry.hub.docker.com/$(cat ${LOCAL_DIR}/coffea-image.txt)
         echo "$(singularity exec ${COFFEA_IMAGE_PATH} python -c 'import fastjet; print(fastjet.__path__[0])')/_fastjet_core/lib"
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(singularity exec ${COFFEA_IMAGE_PATH} python3 -c 'import fastjet; print(fastjet.__path__[0])')/_fastjet_core/lib
